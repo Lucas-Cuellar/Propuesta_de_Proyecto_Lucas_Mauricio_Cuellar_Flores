b66d8a6b06bb6bdc05ba4c68feec202a478c7dd4
Lucas-Cuellar <lucas.cuellar@ucb.edu.bo>
2025-11-28 22:43:30 -0400

    Update config.py

--- Definitive_progra/audio_trainer/config.py
+++ Definitive_progra/audio_trainer/config.py
@@ -23,7 +23,8 @@ AUGMENT_NOISE_STD = 0.01
 MIN_CHUNKS_WARNING = 5
 
 # Modelos
-MODELS_DIR = r"C:\Users\PERSONAL\Desktop\Ultimate_Progra\Definitive_progra\Muestras"
+MODELS_DIR = r"ACA DEBES PONER LA  DIRECCION DE LA CARPETA MUESTRAS"
 
 # Asegura que exista el directorio de modelos
 os.makedirs(MODELS_DIR, exist_ok=True)
+
